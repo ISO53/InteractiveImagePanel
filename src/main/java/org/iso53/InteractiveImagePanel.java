c4e2038ef4ed0f94c6cc832a9ed825f12926cd79
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-04-07 16:58:07 +0300

    imports

--- src/main/java/org/iso53/InteractiveImagePanel.java
+++ src/main/java/org/iso53/InteractiveImagePanel.java
@@ -2,6 +2,8 @@ package org.iso53;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
 public class InteractiveImagePanel extends JPanel {
